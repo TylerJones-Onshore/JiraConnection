0a3e96b90b7a3cd6cbe763076ba1a5617f846a38
TylerJones-Onshore <64983567+TylerJones-Onshore@users.noreply.github.com>
2020-11-03 10:26:35 -0600

    Update GetProjectIssues.java

--- JiraConnection/src/test/java/apiTest/GetProjectIssues.java
+++ JiraConnection/src/test/java/apiTest/GetProjectIssues.java
@@ -7,9 +7,9 @@ import jiraObjects.Issue;
 
 public class GetProjectIssues {
 	public static void main(String[] args) {
-		Client c = new Client("tjones@aarp.org", "0yw8ZD5F9Xn7p2gVAF8pC588");
-		c.baseUri = "https://aarpqmo.atlassian.net";
-		List<Issue> issues = c.getAllIssues("TES",null,null);
+		Client c = new Client("username should go here", "jira api key goes here");
+		c.baseUri = "https://yourcompany.atlassian.net";
+		List<Issue> issues = c.getAllIssues("ProjectKey",null,null);
 
 		System.out.println("BUGS:");
 		for (Issue i : issues) {
