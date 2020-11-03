adf9cbd1d5365f25ee20fcab806e5aac3530ecd0
TylerJones-Onshore <64983567+TylerJones-Onshore@users.noreply.github.com>
2020-11-03 10:25:22 -0600

    Update GetIssue.java

--- JiraConnection/src/test/java/apiTest/GetIssue.java
+++ JiraConnection/src/test/java/apiTest/GetIssue.java
@@ -6,9 +6,9 @@ import jiraObjects.Issue;
 public class GetIssue {
 
 	public static void main(String[] args) {
-		Client c = new Client("tjones@aarp.org","0yw8ZD5F9Xn7p2gVAF8pC588");
-		c.baseUri = "https://aarpqmo.atlassian.net";
-		Issue issue = c.getIssue("TES-8");
+		Client c = new Client("username should go here", "jira api key goes here");
+		c.baseUri = "https://yourcompany.atlassian.net";
+		Issue issue = c.getIssue("Jira Issue Key");
 		System.out.println(issue);
 	}
 }
