1a85155ecf4d86a66af55c2341412d82ad481fc5
TylerJones-Onshore <64983567+TylerJones-Onshore@users.noreply.github.com>
2020-11-03 10:29:07 -0600

    Update UpdateIssue.java

--- JiraConnection/src/test/java/apiTest/UpdateIssue.java
+++ JiraConnection/src/test/java/apiTest/UpdateIssue.java
@@ -11,18 +11,18 @@ public class UpdateIssue {
 	public static void main(String[] args) {
 		IssueUpdate issue = new IssueUpdate();
 		//issue is the Jira key
-		issue.issue = "TES-1";
+		issue.issue = "";
 		//assignee must use the account id of the desired assignee
-		issue.assignee = "5bfc155710c30e4ac8c8a502";
-		issue.description = "Using this ticket to test the api connection to JIRA using a java framework. Attempt #2";
+		issue.assignee = "accountId";
+		issue.description = "Using this ticket to test the api connection to JIRA using a java framework.";
 		issue.labels = new String[]{"Regression","Automation"};
 		issue.components = new String[] {"Application","Framework"};
 		issue.fixVersions = "Version 1";
 		//summary is the Title of the ticket.
 		issue.summary = "Testing JIRA API connection";
 		
-		Client c = new Client("tjones@aarp.org","0yw8ZD5F9Xn7p2gVAF8pC588");
-		c.baseUri = "https://aarpqmo.atlassian.net";
+		Client c = new Client("username should go here", "jira api key goes here");
+		c.baseUri = "https://yourcompany.atlassian.net";
 		c.contentType= "application/json";
 		System.out.println(c.updateIssue(issue));
 	}
