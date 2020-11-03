b774b7629c0284380b07a5968525d53c0de00359
TylerJones-Onshore <64983567+TylerJones-Onshore@users.noreply.github.com>
2020-11-03 10:27:56 -0600

    Update IssuesByJQL.java

--- JiraConnection/src/test/java/apiTest/IssuesByJQL.java
+++ JiraConnection/src/test/java/apiTest/IssuesByJQL.java
@@ -10,12 +10,12 @@ import jiraObjects.Issue;
 public class IssuesByJQL {
 	@Test
 	public static void main(String[] args) {
-		Client c = new Client("tjones@aarp.org", "0yw8ZD5F9Xn7p2gVAF8pC588");
-		c.baseUri = "https://aarpqmo.atlassian.net";
-		List<Issue> framework = c.getIssuesByJQL("TES","component = Framework");
-		List<Issue> application = c.getIssuesByJQL("TES","component = Application");
-		List<Issue> notNull = c.getIssuesByJQL("TES","component != null");
-		List<Issue> isNull = c.getIssuesByJQL("TES","component = null");
+		Client c = new Client("username should go here", "jira api key goes here");
+		c.baseUri = "https://yourcompany.atlassian.net";
+		List<Issue> framework = c.getIssuesByJQL("ProjectKey","component = Framework");
+		List<Issue> application = c.getIssuesByJQL("ProjectKey","component = Application");
+		List<Issue> notNull = c.getIssuesByJQL("ProjectKey","component != null");
+		List<Issue> isNull = c.getIssuesByJQL("ProjectKey","component = null");
 
 		System.out.println("framework:");
 		for (Issue i : framework) {
