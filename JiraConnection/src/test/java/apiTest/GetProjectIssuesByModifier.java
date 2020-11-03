6854668f671257fc7d993e98cb216f7714c7a491
TylerJones-Onshore <64983567+TylerJones-Onshore@users.noreply.github.com>
2020-11-03 10:27:04 -0600

    Update GetProjectIssuesByModifier.java

--- JiraConnection/src/test/java/apiTest/GetProjectIssuesByModifier.java
+++ JiraConnection/src/test/java/apiTest/GetProjectIssuesByModifier.java
@@ -10,9 +10,9 @@ import jiraObjects.Issue;
 public class GetProjectIssuesByModifier {
 	@Test
 	public static void main(String[] args) {
-		Client c = new Client("tjones@aarp.org", "0yw8ZD5F9Xn7p2gVAF8pC588");
-		c.baseUri = "https://aarpqmo.atlassian.net";
-		List<Issue> issues = c.getAllIssues("TES","summary","Test");
+		Client c = new Client("username should go here", "jira api key goes here");
+		c.baseUri = "https://yourcompany.atlassian.net";
+		List<Issue> issues = c.getAllIssues("ProjectKey","summary","Test");
 
 		System.out.println("BUGS:");
 		for (Issue i : issues) {
